dec15e66a74d6548272062ceaffa5aa8368b8d3e
jessicapark <jpark32@umbc.edu>
2017-11-30 21:35:36 -0500

    Added Hello World

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,47 @@
 package com.company;
 
-public class Main {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
+public class Main
+{
 
-        String employeeName;
-        double hours, payRate;
+    public static void main(String[] args)
+    {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Employee Level Number from 1 and 4: ");
+
+        String employeeLevel = input.nextLine();
+
+        System.out.println(employeeLevel);
+
+        switch (employeeLevel)
+        {
+            case "1":
+                System.out.println("Register = Low Clearance");
+                break;
+
+            case "2":
+                System.out.println("Store Manager = Low Clearance");
+                break;
+
+            case "3":
+                System.out.println("Branch Manager = Mid Clearance");
+                break;
+
+            case "4":
+                System.out.println("Executive VP = High Clearance");
+                break;
+
+            default:
+                System.out.println("Invalid Entry");
+                System.exit(1);
+        }
 
 
 
     }
 }
+
+
